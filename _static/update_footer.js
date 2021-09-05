2b54c5abdc57cf34745854429dace4801be6c380
Matias Agelvis <Magelvisdoz@gmail.com>
2021-09-04 21:35:28 -0400

    Prevnets footer flex paragraph from overflowing

--- _static/update_footer.js
+++ _static/update_footer.js
@@ -5,5 +5,5 @@ window.onload = function () {
 
 	let original_info = footer_info.innerHTML
 
-	footer_info.innerHTML = "<p style=\"width: 100%; display: flex; justify-content: space-between;\"><span class=\"sd-sphinx-override sd-badge sd-outline-primary sd-text-primary\">Universidad de Los Andes - Mérida, Venezuela</span><a href=\"mailto:agelvis@ula.ve\"><span class=\"sd-sphinx-override sd-badge sd-outline-primary sd-text-primary\">Contacto en: agelvis@ula.ve</span></a><a href=\"https://matiasagelvis.github.io\"><span class=\"sd-sphinx-override sd-badge sd-outline-primary sd-text-primary\">Página mantenida por Matías Agelvis</span></a></p>" + original_info
+	footer_info.innerHTML = "<p style=\"width: 100%; display: flex; justify-content: space-between; flex-flow: row wrap;\"><span class=\"sd-sphinx-override sd-badge sd-outline-primary sd-text-primary\">Universidad de Los Andes - Mérida, Venezuela</span><a href=\"mailto:agelvis@ula.ve\"><span class=\"sd-sphinx-override sd-badge sd-outline-primary sd-text-primary\">Contacto en: agelvis@ula.ve</span></a><a href=\"https://matiasagelvis.github.io\"><span class=\"sd-sphinx-override sd-badge sd-outline-primary sd-text-primary\">Página mantenida por Matías Agelvis</span></a></p>" + original_info
 }
